6bd81d499572754b497d790f0286d4a4cba6114d
Ian Chesal <ian.chesal@gmail.com>
2015-07-26 23:05:26 -0700

    Install PHP 5.6 from webtatic archive

--- base/scripts/php/v5.6/install.sh
+++ base/scripts/php/v5.6/install.sh
@@ -1,4 +1,35 @@
 #!/bin/bash -ex
+# See: https://webtatic.com/packages/php56/
 
 yum -y upgrade
-yum install php56
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+yum -y install \
+  php56w \
+  php56w-bcmath \
+  php56w-common \
+  php56w-dba \
+  php56w-enchant \
+  php56w-fpm \
+  php56w-intl \
+  php56w-mbstring \
+  php56w-mcrypt \
+  php56w-mysql \
+  php56w-odbc \
+  php56w-opcache \
+  php56w-pdo \
+  php56w-pear \
+  php56w-pecl-apcu \
+  php56w-pecl-gearman \
+  php56w-pecl-geoip \
+  php56w-pecl-imagick \
+  php56w-pecl-memcache \
+  php56w-pecl-xdebug \
+  php56w-process \
+  php56w-pspell \
+  php56w-recode \
+  php56w-snmp \
+  php56w-soap \
+  php56w-tidy \
+  php56w-xml \
+  php56w-xmlrpc
