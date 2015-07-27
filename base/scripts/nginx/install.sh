cff3d51d83cbbacf23e65a2d7b459689d98fc209
Ian Chesal <ian.chesal@gmail.com>
2015-07-26 22:33:28 -0700

    And it's not --yes, it's -y...

--- base/scripts/nginx/install.sh
+++ base/scripts/nginx/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
 yum update
-yum --yes install nginx
+yum -y install nginx
 echo "\ndaemon off;" >> /etc/nginx/nginx.conf
 chown -R www-data:www-data /var/lib/nginx
--- base/scripts/percona/v5.6/install.sh
+++ base/scripts/percona/v5.6/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ex
 
-yum --yes remove mysql* && yum --yes autoremove
+yum -y remove mysql* && yum -y autoremove
 yum install http://www.percona.com/downloads/percona-release/redhat/0.1-3/percona-release-0.1-3.noarch.rpm
 yum update
-yum --yes install Percona-Server-client-56 Percona-Server-server-56
+yum -y install Percona-Server-client-56 Percona-Server-server-56
 cp /opt/configs/percona/v5.6/my.cnf /etc/mysql/my.cnf
 
 /usr/sbin/mysqld --user=root &
