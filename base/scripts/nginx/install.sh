45e6ba2f9a98ab3248c5fa7a3f0b1a8d23e2998f
Ian Chesal <ian.chesal@gmail.com>
2015-07-26 22:31:26 -0700

    Put --yes in the right place for yum calls

--- base/scripts/nginx/install.sh
+++ base/scripts/nginx/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
 yum update
-yum install --yes nginx
+yum --yes install nginx
 echo "\ndaemon off;" >> /etc/nginx/nginx.conf
 chown -R www-data:www-data /var/lib/nginx
--- base/scripts/percona/v5.6/install.sh
+++ base/scripts/percona/v5.6/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ex
 
-yum remove --yes mysql* && yum autoremove --yes
+yum --yes remove mysql* && yum --yes autoremove
 yum install http://www.percona.com/downloads/percona-release/redhat/0.1-3/percona-release-0.1-3.noarch.rpm
 yum update
-yum install --yes Percona-Server-client-56 Percona-Server-server-56
+yum --yes install Percona-Server-client-56 Percona-Server-server-56
 cp /opt/configs/percona/v5.6/my.cnf /etc/mysql/my.cnf
 
 /usr/sbin/mysqld --user=root &
